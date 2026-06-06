d364d3af58b34b054cfa1771f9dd141f1b6bc617
shivamkumarsingh09 <shivamsingh43160@gmail.com>
2026-06-06 13:16:26 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (16.77%) - LeetSync

---
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.c
@@ -0,0 +1,11 @@
+int diagonalSum(int** mat, int matSize, int* matColSize) {
+    int sum = 0;
+    for (int i = 0; i < matSize; i++) {
+        sum += mat[i][i];
+        sum += mat[i][matSize - 1 - i];
+    }
+    if (matSize % 2 != 0) {
+        sum -= mat[matSize / 2][matSize / 2];
+    }
+    return sum;
+}
