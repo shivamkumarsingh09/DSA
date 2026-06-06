4b6302e03acbc091d0b4f241f32dadb3d861375c
shivamkumarsingh09 <shivamsingh43160@gmail.com>
2026-06-06 13:30:51 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (66.77%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.c
@@ -0,0 +1,14 @@
+int pivotIndex(int* nums, int numsSize) {
+    int total_sum = 0;
+    int left_sum = 0;
+    for (int i = 0; i < numsSize; i++) {
+        total_sum += nums[i];
+    }
+    for (int i = 0; i < numsSize; i++) {
+        if (left_sum == total_sum - left_sum - nums[i]) {
+            return i;
+        }
+        left_sum += nums[i];
+    }
+    return -1;
+}
